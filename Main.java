612b45ab2940cb56c51664961295c461470886f7
PG-Mazen <mazen.2003.pg.business@gmail.com>
2025-05-08 20:03:37 +0300

    Added my identity

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("This is Abdelrahman");
     System.out.println("This is Abdelrahman");
-
+    System.out.println("This is Mazen");
+    System.out.println("This is Mazen again :)");
   }
 }
